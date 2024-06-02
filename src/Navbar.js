d46b6a2cc96efc0527448bb17c46a5c0556f6ef3
doublexmax <maxxtandon@gmail.com>
2024-06-02 16:26:04 -0700

    whoami animation

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,7 +2,7 @@ function Navbar() {
     return ( 
         <nav className="navbar">
             <img src="MT.png" className="personal-logo"></img>
-            <h2>Maxx Tandon</h2>
+            <h2 className="typewriter"></h2>
             <div className="links">
                 <a href="https://www.linkedin.com/in/maxxtandon/" target="_blank" rel="noreferrer">LinkedIn</a>
                 <a href="https://github.com/doublexmax/" target="_blank" rel="noreferrer">GitHub</a>
